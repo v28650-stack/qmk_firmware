c74771a97be000c7db4d79c8860f46da02c7c6e5
v28650-stack <v28650@gmail.com>
2026-05-27 14:11:48 -0400

    Update config.h

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -2,6 +2,6 @@
 
 #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 4095
 
-// Encoder configuration for AVR (Pro Micro)
-#define ENCODERS_PAD_A { D2 }
-#define ENCODERS_PAD_B { D3 }
+// Encoder configuration for RP2040 Helios
+#define ENCODERS_PAD_A { GP18 }
+#define ENCODERS_PAD_B { GP19 }
