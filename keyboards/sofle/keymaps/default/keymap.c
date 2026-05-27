84b3d48fae77064e9c66e866fd5664f764970e0e
v28650-stack <v28650@gmail.com>
2026-05-27 08:22:08 -0400

    Update keymap.c

--- keyboards/sofle/keymaps/default/keymap.c
+++ keyboards/sofle/keymaps/default/keymap.c
@@ -58,8 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-// Encoder — volume up/down, click to mute
-#if defined(ENCODER_MAP_ENABLE)
+// Encoder map
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_QWERTY]  = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [_COLEMAK] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
