6cee009aef66fb3b70696a13323b7471535a681d
v28650-stack <v28650@gmail.com>
2026-05-27 09:27:11 -0400

    Update rules.mk

--- keyboards/sofle/keymaps/default/rules.mk
+++ keyboards/sofle/keymaps/default/rules.mk
@@ -1,10 +1,8 @@
 VIA_ENABLE = yes
 LTO_ENABLE = yes
 ENCODER_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
 OLED_ENABLE = yes
 WPM_ENABLE = yes
 
-# Hard limit to 4 layers to guarantee VIA fitment
 DYNAMIC_KEYMAP_LAYER_COUNT = 4
 EXTRAKEY_ENABLE = yes
