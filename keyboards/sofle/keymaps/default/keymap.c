57b9d2cd18209171a9590aa584ca6d5eb9a5937b
v28650-stack <v28650@gmail.com>
2026-05-27 11:45:34 -0400

    Update keymap.c

--- keyboards/sofle/keymaps/default/keymap.c
+++ keyboards/sofle/keymaps/default/keymap.c
@@ -1,5 +1,3 @@
-// Copyright 2026 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -10,56 +8,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE, XXXXXXX,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,                    KC_ENT,  MO(2),   KC_RGUI, KC_RALT, KC_RCTL
     ),
+
     [1] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
         _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______
     ),
+
     [2] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______
     ),
+
     [3] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, QK_BOOT,
         _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______
     )
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (clockwise) {
-        tap_code(KC_A);
+        tap_code(KC_VOLU);
     } else {
-        tap_code(KC_B);
+        tap_code(KC_VOLD);
     }
     return false;
 }
-
-#ifdef OLED_ENABLE
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    return OLED_ROTATION_270; 
-}
-
-bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        oled_write_P(PSTR("MS TR\n"), false);
-        oled_write_P(PSTR("Layer\n"), false);
-        switch (get_highest_layer(layer_state)) {
-            case 0:  oled_write_P(PSTR("BASE \n"), false); break;
-            case 1:  oled_write_P(PSTR("LWR  \n"), false); break;
-            case 2:  oled_write_P(PSTR("RSE  \n"), false); break;
-            default: oled_write_P(PSTR("ADJ  \n"), false); break;
-        }
-    } else {
-        oled_write_P(PSTR("SLAVE\n"), false);
-    }
-    return false;
-}
-#endif
