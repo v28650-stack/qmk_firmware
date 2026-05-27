3b54483ea5dadf65b45cf38a3c67644523452af3
v28650-stack <v28650@gmail.com>
2026-05-27 14:14:21 -0400

    Update config.h

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -2,6 +2,13 @@
 
 #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 4095
 
+// Fix for Pico SDK macro conflicts
+#undef MAX
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+// Include Pico SDK pin definitions
+#include "pico/stdlib.h"
+
 // Encoder configuration for RP2040 Helios
 #define ENCODERS_PAD_A { GP18 }
 #define ENCODERS_PAD_B { GP19 }
