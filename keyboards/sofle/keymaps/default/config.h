78e3d536c0c70c0fea4e195ed43b8fc9a610b1f5
v28650-stack <v28650@gmail.com>
2026-05-27 11:28:18 -0400

    Update config.h

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -1,7 +1,9 @@
 #pragma once
 
-// Force QMK's serial driver to use the exact hardware pin mapped to the TRRS/TRS cable jack
-// #define SERIAL_USART_TX_PIN GP1
-
 // Allow layout mutations via VIA
 #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 4095
+
+// Encoder Configuration
+#define ENCODERS_PAD_A { GP18 }
+#define ENCODERS_PAD_B { GP19 }
+#define ENCODER_RESOLUTION 4
