de586793334e86033f3c58bb0ad5d591f714e240
v28650-stack <v28650@gmail.com>
2026-05-27 09:50:17 -0400

    Update config.h

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -5,9 +5,9 @@
 #undef ENCODERS_PAD_B
 #undef ENCODER_RESOLUTION
 
-// Correct virtual pin mappings used by the QMK helios converter
-#define ENCODERS_PAD_A { F5, F4 }
-#define ENCODERS_PAD_B { F4, F5 }
+// Correct native single-pin encoder routes for Sofle Rev1
+#define ENCODERS_PAD_A { F5 }
+#define ENCODERS_PAD_B { F4 }
 #define ENCODER_RESOLUTION 2
 
 // De-register layout maps to enforce direct user callbacks
