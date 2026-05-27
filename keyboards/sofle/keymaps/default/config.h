1a88949248ff8886eef04f08ff2fbf2d09bc11c7
v28650-stack <v28650@gmail.com>
2026-05-27 11:42:41 -0400

    Update config.h

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -2,8 +2,3 @@
 
 // Allow layout mutations via VIA
 #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 4095
-
-// Encoder Configuration
-#define ENCODERS_PAD_A { GP18 }
-#define ENCODERS_PAD_B { GP19 }
-#define ENCODER_RESOLUTION 4
