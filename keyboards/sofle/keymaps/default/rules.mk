4a39f5affdd4bed96c58f1664f28ae5ab1abafe5
v28650-stack <v28650@gmail.com>
2026-05-27 08:31:56 -0400

    Update rules.mk

--- keyboards/sofle/keymaps/default/rules.mk
+++ keyboards/sofle/keymaps/default/rules.mk
@@ -1,5 +1,9 @@
-TRI_LAYER_ENABLE = yes
+# Hardware Features
+VIA_ENABLE = yes
+LTO_ENABLE = yes
 ENCODER_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
 OLED_ENABLE = yes
 WPM_ENABLE = yes
+
+# Avoid firmware size overflow on RP2040
+EXTRAKEY_ENABLE = yes
