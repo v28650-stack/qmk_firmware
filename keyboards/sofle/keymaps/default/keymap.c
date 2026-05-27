435ed357595782d77df0688352ceac8d7730771e
v28650-stack <v28650@gmail.com>
2026-05-27 10:11:06 -0400

    Update keymap.c

--- keyboards/sofle/keymaps/default/keymap.c
+++ keyboards/sofle/keymaps/default/keymap.c
@@ -57,19 +57,13 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_270; 
 }
 
-bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        oled_write_P(PSTR("MS TR\n"), false);
-        oled_write_P(PSTR("Layer\n"), false);
-        switch (get_highest_layer(layer_state)) {
-            case 0:  oled_write_P(PSTR("BASE \n"), false); break;
-            case 1:  oled_write_P(PSTR("LWR  \n"), false); break;
-            case 2:  oled_write_P(PSTR("RSE  \n"), false); break;
-            default: oled_write_P(PSTR("ADJ  \n"), false); break;
-        }
-    } else {
-        oled_write_P(PSTR("SLAVE\n"), false);
-    }
-    return false;
-}
+// Put this at the very bottom of your keymap.c file:
+
+#ifdef ENCODER_MAP_ENABLE
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
+};
 #endif
