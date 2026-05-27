898d09d00ecf04dd20ce59100f05a9307cf575f7
v28650-stack <v28650@gmail.com>
2026-05-27 14:20:54 -0400

    Update config.h

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -6,9 +6,6 @@
 #undef MAX
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
-// Include Pico SDK pin definitions
-#include "pico/stdlib.h"
-
 // Encoder configuration for RP2040 Helios
 #define ENCODERS_PAD_A { GP18 }
 #define ENCODERS_PAD_B { GP19 }
