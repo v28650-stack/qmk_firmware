3565b34786b878f8f1eba0b76ceaea37224947fb
v28650-stack <v28650@gmail.com>
2026-05-27 09:17:06 -0400

    Update config.h

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -1,7 +1,4 @@
-// Copyright 2024 Santosh Kumar (@santosh)
-// SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
-#define TRI_LAYER_LOWER_LAYER 2
-#define TRI_LAYER_UPPER_LAYER 3
-#define TRI_LAYER_ADJUST_LAYER 4
-#define TAP_CODE_DELAY 10
+
+// Force QMK to recognize custom programmable encoder mappings over base layout definitions
+#define ENCODERS_USER_DEFINED
