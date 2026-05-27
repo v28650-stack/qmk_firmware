9c67fdf7631aff8c3c15a883066e4b111a7e11a2
v28650-stack <v28650@gmail.com>
2026-05-27 09:27:27 -0400

    Update keymap.c

--- keyboards/sofle/keymaps/default/keymap.c
+++ keyboards/sofle/keymaps/default/keymap.c
@@ -1,64 +1,20 @@
-// Copyright 2026 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
-#include QMK_KEYBOARD_H
+// Leave your entire const uint16_t PROGMEM keymaps array completely untouched here!
+// Just paste the code below at the very bottom of the file, replacing the old encoder lines:
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE, XXXXXXX,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,                    KC_ENT,  MO(2),   KC_RGUI, KC_RALT, KC_RCTL
-    ),
-    [1] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-        _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______
-    ),
-    [2] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______
-    ),
-    [3] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, QK_BOOT,
-        _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______
-    )
-};
-
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-    [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
-};
-#endif
-
-#ifdef OLED_ENABLE
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    return OLED_ROTATION_270; 
-}
-
-bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        oled_write_P(PSTR("MS TR\n"), false);
-        oled_write_P(PSTR("Layer\n"), false);
-        switch (get_highest_layer(layer_state)) {
-            case 0:  oled_write_P(PSTR("BASE \n"), false); break;
-            case 1:  oled_write_P(PSTR("LWR  \n"), false); break;
-            case 2:  oled_write_P(PSTR("RSE  \n"), false); break;
-            default: oled_write_P(PSTR("ADJ  \n"), false); break;
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { // Left Side Encoder
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
         }
-    } else {
-        oled_write_P(PSTR("SLAVE\n"), false);
+    } else if (index == 1) { // Right Side Encoder
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
     }
     return false;
 }
